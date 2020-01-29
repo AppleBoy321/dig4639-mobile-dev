00b6f05e6d693f6df340dc6324ff3875892a0a2c
AppleBoy321 <59659125+AppleBoy321@users.noreply.github.com>
2020-01-29 12:28:31 -0500

    Co-authored-by: airbender106 <sdharmakirthi@Knights.ucf.edu>

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -1,8 +1,8 @@
 import Component from "./Component.js";
-import Task from "./Task.js";
+import Task from "./task.js";
 function runOnLoad()
 {
-    
+    console.log("New change");
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
@@ -11,6 +11,17 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task(
+        {
+            content: "Finish Ice Exercise",
+            done: false
+        }
+    );
+    var container = document.getElementById("tasklist");
+    var newtask = document.createElement("div");
+    newtask.innerHTML = myTask.render();
+    container.appendChild(newtask);
+    console.log(myTask.render());
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
