9be958622c8c341163b643750e3f4b8dafe7a949
AppleBoy321 <59659125+AppleBoy321@users.noreply.github.com>
2020-02-12 11:43:54 -0500

    Updates

---
+++ precache-manifest.9b47643c837412b48358f28c092f4065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b287275efea39d137c91f76747ff48",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "e1d3707b651242ec3570",
+    "url": "/dig4639-mobile-dev/static/css/main.c44e0421.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d3707b651242ec3570",
+    "url": "/dig4639-mobile-dev/static/js/main.c93da24a.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
