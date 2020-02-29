dc6a1f69a99f5a7df97d197dd374713b53b8e931
AppleBoy321 <59659125+AppleBoy321@users.noreply.github.com>
2020-02-29 17:48:57 -0500

    lab 4 finished

---
+++ labs/4/src/Components/Card/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+
+class Card extends React.Component {
+  render() {
+    return(
+    <body>
+      <div className="card">
+        <span className="close" 
+        onClick={this.props.dataclick} 
+        datatitle={this.props.title}>&times;</span>
+        <br></br>
+        <h3>{this.props.title}</h3>
+        <p>{this.props.content}</p>
+      </div>
+    </body>
+    );
+  }
+
+}
+
+export default Card;
